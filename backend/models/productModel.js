37310e385a5fc1647db00ac3715d18bc97e375b7
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2020-12-29 17:57:39 +0400

    f

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 const productSchema = mongoose.Schema({
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     category: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
