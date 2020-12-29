509b67a03c424267bfa8c5c7b019c72e33bc83c2
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2020-12-30 00:08:50 +0400

    h

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,27 +2,37 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className='welcome-div row center'>
-            <h1 className='about row center' style={{ fontSize: "3rem" }} >
-                Rabee Aljaramani<br />
-                Web developer<br />
-                (+971) 567657124<br />
+        <div className='welcome-div'>
+            <h1 className='about' style={{ fontSize: "3rem" }} >
+                Rabee Aljaramani -
+                Web developer :<br />
+
+                <div className='about-div'>
+                    <h1 className="aboutme">
+                        First-rate Informatics Engineer with +9 years of experience offering outstanding support to users and companies
+                        of various levels in different countries. Passionate about web development and seeking to grow in the role of a
+                        web developer by using my React, HTML and CSS skills to deliver coding excellence. Ambitious problem solver
+                        with a passion for online businesses and would like to join a team of likeminded developers. Constantly striving to
+                        improve myself and enhance my skills in technologies.
+                    </h1>
+                </div>
+            </h1>
+
+            <br />
+            <h1 className='about' style={{ fontSize: "3rem" }} >
+                Contact :
+                    <br />
+                <div className='about-div'>
+                    <h1 className="aboutme">
+                        Mobile:     (+971) 567657124<br />
                 Email address: rabeealjaramani@yahoo.com<br />
                 Nationality: Syrian<br />
                 Address : Dubai (United Arab Emirates)<br />
-                <h1 className="aboutme" style={{ backgroundColor: "#FAEBD7", width: "70%", borderRadius: "1rem" }}>
-                    First-rate Informatics Engineer with +9 years of experience offering outstanding support to users and companies
-                    of various levels in different countries. Passionate about web development and seeking to grow in the role of a
-                    web developer by using my React, HTML and CSS skills to deliver coding excellence. Ambitious problem solver
-                    with a passion for online businesses and would like to join a team of likeminded developers. Constantly striving to
-                    improve myself and enhance my skills in technologies.
-</h1>
-
-
+                    </h1>
+                </div>
             </h1>
 
 
-
-        </div>
+        </div >
     )
 }
