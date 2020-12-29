931af4ce8d0406252e2ca0d216e98c112f8294f2
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2020-12-29 22:55:56 +0400

    g

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome() {
     return (
         <div className='welcome-div row center'>
             <h1 className='welcome-h1' >
-                We are not alon, but we are the best
+                We are not alone, but we are the best
                 </h1>
 
 
